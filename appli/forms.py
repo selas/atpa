cb6b436c53b943599724101c6947fb4690eafbd6
Franck Mallart <mallart@info26-16.iut45.univ-orleans.fr>
2014-06-19 17:05:31 +0200

    Ajout meilleur fonctionnalité pour le temps de réponse lors de la création d'une question

--- appli/forms.py
+++ appli/forms.py
@@ -9,15 +9,28 @@ class Connexion(forms.Form):
 
 class AjoutQuestion(forms.Form):
 	mesTypes = Type.objects.all()
+
 	CHOICES = []
 	for monType in mesTypes :
 		CHOICES.append([monType.id, monType.libelle])
 
+	HEURE = []
+	for i in range(0,24) :
+		HEURE.append([i,i])
+
+	MINSEC = []
+	for i in range(0,60) :
+		MINSEC.append([i,i])
+
 	intituleQuestion = forms.CharField(widget = forms.Textarea(attrs = {'rows' : '3', 'style' : 'resize:vertical', 'class' : 'form-control'}))
 	
-	temps = forms.TimeField(label="Temps pour répondre", required=True, input_formats=("%M:%S",))
+	temps = forms.TimeField(widget=forms.TimeInput(format='%H:%M:%S', attrs = {'class':'form-control input-sm', 'type':'hidden'}), label="Temps pour répondre", required=True)
 
 	typeQuestion = forms.ChoiceField(widget=forms.Select(attrs = {'class':'form-control input-sm'}), choices=CHOICES)
+
+	listeHeure = forms.ChoiceField(widget=forms.Select(attrs = {'class':'input-sm'}), choices=HEURE)
+	listeMinute = forms.ChoiceField(widget=forms.Select(attrs = {'class':'input-sm'}), choices=MINSEC)
+	listeSeconde = forms.ChoiceField(widget=forms.Select(attrs = {'class':'input-sm'}), choices=MINSEC)
 
 	intituleReponseBonne1 = forms.CharField(max_length = 100)
 	intituleReponseMauvaise1 = forms.CharField(max_length = 100)
